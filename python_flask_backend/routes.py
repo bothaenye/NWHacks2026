cb6e7ad032b955f8377dcc5f476e9dd5e450b274
Riley <RWMB4200@gmail.com>
2026-01-17 15:23:48 -0800

    Added route initialization to the posture main file

--- python_flask_backend/routes.py
+++ python_flask_backend/routes.py
@@ -1,10 +1,13 @@
+# Main router file
 
+
+# ROUTE SYNTAX
 # @app.route("path", methods=["METHOD"])
 # def foobar():
 #   ...
 
 
 def init_routes(app):
-    @app.route("/", methods=["GET"])
+    @app.route("/", methods=["POST"])
     def yo():
         return "Yo"
