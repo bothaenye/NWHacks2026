9f6822f5069f12b4233f2d3fdd9fe47caceea69d
Riley <RWMB4200@gmail.com>
2026-01-18 08:20:43 -0800

    Render stuff

--- python_flask_backend/posture_main_file.py
+++ python_flask_backend/posture_main_file.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
 from events import init_events
-
+import os
 from routes import init_routes
 
 app = Flask(__name__)
@@ -11,4 +11,5 @@ init_events(socketio)
 init_routes(app)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
